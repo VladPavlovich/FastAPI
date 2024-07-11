af1d339e9a58e0d038d655da0e6c6073ac6c45b9
vmpavlov <vmpavlov@iu.edu>
2024-07-11 13:17:21 -0500

    updated with transcript

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 class Name(BaseModel):
     name: str
 
+class Transcription(BaseModel):
+    transcription: str
+
 recognized_names = []
+transcriptions = []
 
 @app.post("/names/")
 def add_name(name: Name):
@@ -16,6 +20,15 @@ def add_name(name: Name):
 @app.get("/names/")
 def get_names():
     return {"recognized_names": recognized_names}
+
+@app.post("/transcriptions/")
+def add_transcription(transcription: Transcription):
+    transcriptions.append(transcription.transcription)
+    return {"message": "Transcription added successfully", "current_transcriptions": transcriptions}
+
+@app.get("/transcriptions/")
+def get_transcriptions():
+    return {"transcriptions": transcriptions}
 
 @app.get("/")
 def read_root():
